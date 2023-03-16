aa433cb7001d82dbbe3246f6467cd6124ac6bca9
Oguzhan Ozgul <oguzhanozgul@gmail.com>
2023-03-16 17:22:56 +0100

    Sticky search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,8 +4,16 @@ import { Box, Center, Group } from "@mantine/core";
 
 export function SearchBar() {
   return (
-    <Box h="100%">
-      <Center h="100%" w="100%" mx="auto" p="md">
+    <Box
+      mt={32}
+      p={8}
+      mx="auto"
+      bg="#0C090D"
+      sx={{
+        position: "sticky", top: "10px", zIndex: 10, borderRadius: 8,
+      }}
+    >
+      <Center h="100%" w="100%" p="md">
         <Group position="center">
           <SearchAutocomplete type="from" />
           <SearchAutocomplete type="to" />
